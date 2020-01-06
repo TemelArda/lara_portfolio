62e6a7d6466363c4aec37433efa4105e4a9ded91
ArdaTemel <49353245+TemelArda@users.noreply.github.com>
2020-01-06 14:13:14 -0500

    fixed some issues

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+require('typeface-roboto-slab');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
